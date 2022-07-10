87f24bd5712267b8b90153eb0d2baca220c9ae42
Sang Hyun Park <itkdgus489@gmail.com>
2022-07-10 22:18:33 +0900

    useNavigate

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Lin, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./Screen/Detail";
 import Home from "./Screen/Home";
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Kuhell</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
